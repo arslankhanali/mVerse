b52126671c1b3207fc011481b93868bb923cd7da
arslankhanali <arslankhanali@gmail.com>
2020-05-26 02:48:56 +1000

    YT trailers and imdb added

--- lib/pages/movieDetails.dart
+++ lib/pages/movieDetails.dart
@@ -1,22 +1,36 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:flutube/flutube.dart';
 // import 'package:mVerse/services/movie_class.dart';
+import 'package:mVerse/services/torrent.dart';
 import 'package:mVerse/services/movie.dart';
 import 'package:mVerse/state/app_state.dart';
 import 'package:mVerse/redux/reducers.dart';
 import 'package:mVerse/redux/actions.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
 
 class MovieDetails extends StatefulWidget {
   final Movie m;
 
-  const MovieDetails({Key key, this.m});
+  //final flutterWebviewPlugin = new FlutterWebviewPlugin();
+
+  MovieDetails({Key key, this.m});
 
   @override
   _State createState() => _State();
 }
 
 class _State extends State<MovieDetails> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+  final List<String> listy = ['1', '2'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,33 +59,47 @@ class _State extends State<MovieDetails> {
           padding: EdgeInsets.only(top: 10.0),
           children: <Widget>[
             poster(),
+
+            getTorrentButtons(widget.m),
+
             SizedBox(height: 5.0),
-            boxOne(),
-            boxSecond(),
-            // Text('https://www.youtube.com/watch?v=${widget.m.ytTrailerCode}'),
-           
-            // //https://www.youtube.com/watch?v=YzPq8uVgLe8
-            // Container(
-            //   margin: EdgeInsets.all(10.0),
-            //   width: 325.0,
-            //   height: 300,
-            //   decoration: BoxDecoration(
-            //     color: Colors.white,
-            //     borderRadius: BorderRadius.circular(15.0),
-            //   ),
-            //   child: widget.m.ytTrailerCode!=null?
-            //   FluTube(
-            //     'https://www.youtube.com/watch?v=${widget.m.ytTrailerCode}',
-            //     autoInitialize: true,
-            //     aspectRatio: 16 / 9,
-            //     autoPlay: true,
-            //     looping: true,
-            //     onVideoStart: () {},
-            //     onVideoEnd: () {},
-            //   ):
-            //   Text('Not Available'),
-              
-            // ),
+            //boxOne(),
+            //boxSecond(),
+            Container(
+                margin: EdgeInsets.only(left: 10.0, right: 10),
+                width: 325.0,
+                height: 355,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).accentColor,
+                  // borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: WebView(
+                  initialUrl:
+                      "https://www.youtube.com/watch?v=${widget.m.ytTrailerCode}",
+                  javascriptMode: JavascriptMode.unrestricted,
+                  // onWebViewCreated: (WebViewController webViewController) {
+                  //       _controller.complete(webViewController);
+                  // },
+                )),
+            SizedBox(height: 10),
+            Container(
+                margin: EdgeInsets.only(left: 10, right: 10),
+                width: 325.0,
+                height: 5000,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).accentColor,
+                  // borderRadius: BorderRadius.circular(30.0),
+                ),
+                child: WebView(
+                  initialUrl: "https://www.imdb.com/title/${widget.m.imdbCode}",
+                   gestureNavigationEnabled:true,
+                  //javascriptMode: JavascriptMode.unrestricted,
+                  // onWebViewCreated: (WebViewController webViewController) {
+                  //       _controller.complete(webViewController);
+                  // },
+                ))
+
+
           ],
         ));
   }
@@ -149,5 +177,59 @@ MPA rating: ${widget.m.mpaRating}
         ),
       ),
     );
+  }
+
+  Widget getTorrentButtons(Movie m) {
+    List<Widget> list = new List<Widget>();
+
+    for (var i = 0; i < m.torrents.length; i++) {
+      list.add(
+        new FlatButton(
+          child: Text('${m.torrents[i].quality}'),
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('${m.torrents[i].quality}'),
+                    content: Text("""
+Seeds: ${m.torrents[i].seeds}
+Peers: ${m.torrents[i].peers}
+Size: ${m.torrents[i].size}
+URL: ${m.torrents[i].url}
+                    """),
+                    actions: <Widget>[
+                      FlatButton.icon(
+                        icon:Icon(Icons.link),
+                        label: Text('link'),
+                        onPressed: () {
+                          Clipboard.setData(new ClipboardData(text: '${m.torrents[i].url}'));
+                        },
+                      ),
+                      FlatButton.icon(
+                        icon:Icon(Icons.content_copy),
+                        label: Text('Copy'),
+                        onPressed: () {
+                          Clipboard.setData(new ClipboardData(text: '${m.torrents[i].url}'));
+                        },
+                      ),
+                      FlatButton.icon(
+                        icon:Icon(Icons.close),
+                        label: Text(''),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      
+                    ],
+                  );
+                });
+          },
+        ),
+      );
+    }
+    ;
+    return new Row(mainAxisAlignment: MainAxisAlignment.center,
+    children: list);
   }
 }
